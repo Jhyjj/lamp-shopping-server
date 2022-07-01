a58b3ce797b3fc1df626b2d9850d94d94fc362a5
hyunji <jhyj3768@gmail.com>
2022-07-01 14:42:09 +0900

    sqlite 테이블생성

---
+++ models/product.js
@@ -0,0 +1,26 @@
+//commonjs 구문 내보내기
+//module.exports
+//테이블을 모델링하는 파일
+module.exports = function(sequelize,DataTypes){
+    //컬럼 name,price,imageUrl,seller
+    //제약조건 allowNull : 컬럼의 값이 없어도 되는지 여부(defalut : true)
+    const product = sequelize.define('Product',{
+        name:{
+            type:DataTypes.STRING(200),
+            allowNull : false
+        },
+        price:{
+            type:DataTypes.INTEGER(10),
+            allowNull : false
+        },
+        imgsrc:{
+            type:DataTypes.STRING(500),
+        },
+        seller:{
+            type:DataTypes.STRING(200),
+            allowNull : false
+        },
+    }); 
+       
+    return product;
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const modles = require('./models');
 
 //json 형식의 데이터를 처리할 수 있게 설정하기
 app.use(express.json());
@@ -11,68 +12,41 @@ app.use(cors());
 //요청처리
 //app.메서드(경로,응답(콜백함수로 작성))
 app.get('/products',async(req,res)=>{
-    const result = {
-        products: [
-            {
-                id: 1,
-                name: "진짜 밝은 거실조명",
-                price : 55000,
-                imgsrc:"images/products/product1.jpg",
-                seller : "green"
-            },
-            {
-                id:2,
-                name: "그저 그런 화장실 조명",
-                price: 45000,
-                imgsrc:"images/products/product2.jpg",
-                seller : "green"
-            },
-            {
-                id:3,
-                name: "부엌 조명",
-                price: 45000,
-                imgsrc:"images/products/product3.jpg",
-                seller : "green"
-            },
-            {
-                id:4,
-                name: "거실 간접등",
-                price: 45000,
-                imgsrc:"images/products/product4.jpg",
-                seller : "green"
-            },
-            {
-                id:5,
-                name: "현관 조명",
-                price: 45000,
-                imgsrc:"images/products/product4.jpg",
-                seller : "green"
-            },
-            {
-                id:6,
-                name: "베란다 간접등",
-                price: 45000,
-                imgsrc:"images/products/product3.jpg",
-                seller : "green"
-            },
-            {
-                id:7,
-                name: "안방 스탠드",
-                price: 45000,
-                imgsrc:"images/products/product2.jpg",
-                seller : "green"
-            },
-            {
-                id:8,
-                name: "독서등",
-                price: 45000,
-                imgsrc:"images/products/product1.jpg",
-                seller : "green"
-            }
-        ]
-    }
-    res.send(result);
+   
+    //데이터베이스 조회하기
+    modles.Product.findAll()
+    .then(result=>{
+        console.log("제품전체조회",result);
+        res.send(result);
+    })
+    .catch(e=>{
+        console.error(e)
+        res.send("파일조회에 문제가 있습니다.")
+    })
+
+    
 })
+//method get전송이고, url은 /product/2 로 요청이 왔을때
+app.get('/product/:id', async (req,res)=>{ //id를 파라미터 방식으로 받겠다.
+    console.log(req);
+    const params = req.params;
+    const {id} = params;
+    //하나만 조회할때 findOne --> select문
+    modles.Product.findOne({
+        //조건절
+        where:{
+            id:id
+        }
+    })
+    .then(result=>{
+        res.send(result);
+    })
+    .catch(e=>{
+        console.error(e)
+        res.send("상품조회에 문제가 생겼습니다.")
+    })
+        
+}) 
 app.post('/green',async(req,res)=>{
     console.log(req);
     res.send('그린게시판에 게시글이 등록되었습니다.');
@@ -81,4 +55,17 @@ app.post('/green',async(req,res)=>{
 //실행
 app.listen(port,()=>{
     console.log('쇼핑몰 서버가 동작중입니다.');
+    //sequelize와 데이터베이스 연결 작업
+    //데이터베이스 동기화
+    modles.sequelize
+    .sync()
+    .then(()=>{
+        console.log('db연결 성공~~~~~');
+    })
+    .catch(e=>{
+        console.error(e);
+        console.log('db연결 에러');
+        //서버실행이 안되면 프로세스를 종료
+        process.exit();  
+    })
 })
